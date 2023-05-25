38d5aa10f4b085fe91f543fdda6e57c4026534cd
TazTheTerrible <104862012+TazTheTerrible@users.noreply.github.com>
2023-05-24 20:45:40 -0400

    Update BST.c

--- BST.c
+++ BST.c
@@ -38,3 +38,54 @@ bstNode* Insert(bstNode** root, int data){
     }
     return *root;
 }
+int CountNodes(bstNode* root, int* count){
+    if (root == NULL){
+        return 0;
+    }
+    (*count)++;
+    CountNodes(root->left, count);
+    CountNodes(root->right, count);
+    return *count;
+}
+
+bool IsInTree(bstNode* root, int data){
+    if (root == NULL){
+        return false;
+    } else if (data == root->data){
+        return true;
+    } else if (data < root->data){
+        return IsInTree(root->left, data);
+    } else {
+        return IsInTree(root->right, data);
+    }
+}
+
+void DeleteTree(bstNode* root){
+    if (root == NULL){
+        return;
+    }
+    DeleteTree(root->left);
+    DeleteTree(root->right);
+    free(root);
+    return;
+}
+
+void PrintMinToMax(bstNode* root){
+    if (root == NULL){
+        return;
+    }
+    PrintMinToMax(root->left);
+    printf("%d", root->data);
+    PrintMinToMax(root->right);
+}
+
+int GetHeight(bstNode* root){
+    if (root == NULL){
+        return -1;
+    }
+    int leftHeight = GetHeight(root->left);
+    int rightHeight = GetHeight(root->right);
+    int maxHeight = (leftHeight > rightHeight) ? leftHeight : rightHeight;
+    return maxHeight + 1;
+}
+
